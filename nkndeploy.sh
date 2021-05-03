16a9ad4b423dba1d2c0ab6414e2153d300616702
no112358 <75950250+no112358@users.noreply.github.com>
2021-05-03 13:20:22 +0200

    Update nkndeploy.sh

--- nkndeploy.sh
+++ nkndeploy.sh
@@ -807,8 +807,7 @@ while IFS= read -r file; do
 				days="d "
 				hours="h"
 				output4="$outputDays$days$outputHours$hours"
-				minedblocks=$(printf "%s" "$nkncOutput" | sed -n '/proposalSubmitted/p' | cut -d' ' -f2 | sed -e 's/[",]//g')
-				output5=((minedblocks * 11.09))
+				output5=$(printf "%s" "$nkncOutput" | sed -n '/proposalSubmitted/p' | cut -d' ' -f2 | sed -e 's/[",]//g')
 				
 				# print out in colums
                 printf "%-17s%-18s%-9s%-10s%-10s%-10s\n" "$file" "$output1" "$output2" "$output3" "$output4" "$output5"
@@ -1190,5 +1189,5 @@ mode="whatever"
 database="whatever"
 installation="whatever"
 PUBLIC_IP=$(wget http://ipecho.net/plain -O - -q ; echo)
-version="1.4.1 dev 21"
+version="1.4.1 dev 22"
 menu
