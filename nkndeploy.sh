03c6d320f068578ece0aefd4c49b9ea2bf9e9b5a
no112358 <75950250+no112358@users.noreply.github.com>
2021-05-13 15:00:44 +0200

    Update nkndeploy.sh

--- nkndeploy.sh
+++ nkndeploy.sh
@@ -1286,21 +1286,24 @@ EOF
 
 # Public IP and script version
 PUBLIC_IP=$(wget -q http://ipecho.net/plain -O -)
-version="1.6.0 dev 1"
+version="1.6.0 dev 2"
 
 # NKN-commercial URL selector
 arch=$(uname -m)
-if [[ $arch == "x86_64"]]; then
+# 64bit CPUs
+if [[ $arch == "x86_64" ]]; then
 	nknsoftwareURL="https://commercial.nkn.org/downloads/nkn-commercial/linux-amd64.zip"
 	printf "%s\n" "$arch"
 	printf "%s\n" "$nknsoftwareURL"
 	sleep 5
-elif [[ $arch == "i686"]]; then
+# Older CPUs with 32 bit
+elif [[ $arch == "i686" ]]; then
 	nknsoftwareURL="https://github.com/nknorg/nkn/releases/download/v2.1.3/linux-386.zip"
 	printf "%s\n" "$arch"
 	printf "%s\n" "$nknsoftwareURL"
 	sleep 5
-elif [[ $arch == "armv7l"]]; then
+# Raspberry Pi
+elif [[ $arch == "armv7l" ]]; then
 	nknsoftwareURL="https://github.com/nknorg/nkn/releases/download/v2.1.3/linux-armv7.zip"
 	printf "%s\n" "$arch"
 	printf "%s\n" "$nknsoftwareURL"
