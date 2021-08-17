eb8c39358037b78a27820d2018ec84b62c4b517d
no112358 <75950250+no112358@users.noreply.github.com>
2021-08-17 11:04:25 +0200

    test

--- nkndeploy.sh
+++ nkndeploy.sh
@@ -39,7 +39,8 @@ ufw --force enable > /dev/null 2>&1
 printf "DONE!\n"
 
 # Download the ChainDB archive from nkn.org
-websource="https://nkn.org/ChainDB_pruned_latest.tar.gz" # NKN.org ChainDB URL
+#websource="https://nkn.org/ChainDB_pruned_latest.tar.gz" # NKN.org ChainDB URL
+websource="http://94.237.27.39/ChainDB.tar.gz" # TEST ChainDB URL
 cd /var/www/html/ > /dev/null 2>&1 || exit
 
 printf "Downloading ChainDB archive............................................. \n"
@@ -1338,7 +1339,7 @@ EOF
 
 # Public IP and script version
 PUBLIC_IP=$(wget -q http://ipecho.net/plain -O -)
-version="1.6.1"
+version="1.6.2"
 
 # Detect architecture and select proper NKN-commercial version/URL
 arch=$(uname -m)
